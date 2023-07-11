8992eccd83d067054dbba0274793d5ec67fb1295
saibokka <bokkasai19977@gmail.com>
2023-07-11 13:22:31 +0530

    table records added as sql insert files

---
+++ book.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM ems.book
+LIMIT 0, 1000
+
+-- Date: 2023-07-11 13:14
+*/
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (1,'chethan tt','onegirl',3);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (2,'chetan','thesky',2);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (3,'chakra','redroses',3);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (4,'kumar','thelife',1);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (5,'chakra','patience',2);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (6,'sadguru','happysoul',1);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (7,'kumar','themurder',4);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (202,'chethan b','onegirl',1);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (203,'paul b','demon',4);
+INSERT INTO `` (`book_id`,`author`,`title`,`genres_id`) VALUES (204,'paul b','demon 2',4);
---
+++ genre.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM ems.genres
+LIMIT 0, 1000
+
+-- Date: 2023-07-11 13:13
+*/
+INSERT INTO `` (`genres_id`,`genre`) VALUES (1,'fiction');
+INSERT INTO `` (`genres_id`,`genre`) VALUES (2,'crime');
+INSERT INTO `` (`genres_id`,`genre`) VALUES (3,'romance');
+INSERT INTO `` (`genres_id`,`genre`) VALUES (4,'thriller');
